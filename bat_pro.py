de6b97ee1d7aed917f7a11676729ba15999abee8
Maran8394 <maran8394@gmail.com>
2021-06-10 20:54:56 +0530

    init

--- bat_pro.py
+++ bat_pro.py
@@ -5,27 +5,31 @@ import sys
 
 
 
-def run_code():
-    increment = 0
+def run_code(percentage, power_status):
+    next_ten_min = 0
+    print(next_ten_min)
     while not power_status:
         current_minute = time.localtime().tm_min
 
-        if current_minute == increment:
+        if current_minute == next_ten_min:
             Notification(
                 title="Battery Notification",
                 description="{}% and {}min".format(percentage, current_minute),
                 duration=5,
             ).send()
         else:
-            time.sleep(6)
-            
-        increment = current_minute + 1
+            time.sleep(600) # for 10min sleep
+        print("current_minute", current_minute)    
+        next_ten_min = current_minute + 10
+        print("next_ten_min",next_ten_min)
 
 
-ar = sys.argv[1]
-print(ar)
 
-percentage = sensors_battery().percent
-power_status = sensors_battery().power_plugged  
-if ar == 'start':
-    run_code()
+percentage = sensors_battery().percent # to get percentage of the battery
+power_status = sensors_battery().power_plugged  # to get the power status which is plugged or unplugged
+
+start_arg = sys.argv[1]
+if start_arg == 'start':
+    run_code(percentage, power_status)
+else:
+    print("Enter valid argument")
