3fd87928d748dc3cbc45d8fa09981f866e0dc26a
Maran8394 <maran8394@gmail.com>
2021-06-11 16:25:29 +0530

    First_Commit

--- bat_pro.py
+++ bat_pro.py
@@ -1,27 +1,47 @@
-from psutil import sensors_battery
+from psutil import sensors_battery # for Battery details
 import time
-from pynotifier import Notification
+from pynotifier import Notification # for notification
 import sys
 
 
 
 def run_code(percentage, power_status):
-    next_ten_min = 0
-    print(next_ten_min)
-    while power_status:
-        current_minute = time.localtime().tm_min
+    try:
+        next_ten_min = 0
+        print("SCRIPT STARTED")
+        
+        # print(next_ten_min)
+        while power_status:
+            current_minute = time.localtime().tm_min
 
-        if current_minute == next_ten_min:
-            Notification(
-                title="Battery Notification",
-                description="{}% and {}min".format(percentage, current_minute),
-                duration=5,
-            ).send()
+            if current_minute == next_ten_min:
+                Notification(
+                    title="Battery Notification",
+                    description="{}% and {}min".format(percentage, current_minute),
+                    duration=5,
+                ).send()    
+
+            else:
+                
+                print("current_minute", current_minute)    
+                next_ten_min = current_minute + 1
+                print("next_ten_min",next_ten_min)
+                time.sleep(59)
+
         else:
-            time.sleep(120) # for 10min sleep
-        print("current_minute", current_minute)    
-        next_ten_min = current_minute + 2
-        print("next_ten_min",next_ten_min)
+            Notification(
+                    title="Battery on Charging",
+                    description="This script is developed for only on off-charging ",
+                    duration=5,
+                ).send()
+            print("ENDED")
+
+
+    except KeyboardInterrupt as e:
+        print("You're stop the script.....")
+
+    except:
+        print("Some malfunction occured.....")
 
 
 
